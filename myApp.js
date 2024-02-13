8ac2d8620c4362584bcbea6f6174a8a525b72331
AsaMitaka <cepelits@gmail.com>
2024-02-13 20:52:30 +0200

    + public path

--- myApp.js
+++ myApp.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const PORT = 3000;
 
-const publicPath = path.join(__dirname, 'public');
-app.use(express.static(publicPath));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-  const dirPath = path.join(__dirname, '/views/index.html');
-  res.sendFile(dirPath);
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
 });
 
 module.exports = app;
